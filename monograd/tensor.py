b0fceb9a0c768d853ce86ec573ef8adc34d34b54
lipeeeee <filipe.pereirafga@gmail.com>
2025-12-31 01:48:09 +0000

    tensor attributes & poetry init

--- monograd/tensor.py
+++ monograd/tensor.py
@@ -5,17 +5,41 @@
 #.op Add, Mul, Conv, etc.    Remembers how this tensor was created.
 #.parents    List of Tensors Tells the Autograd where to send the gradients next.
 
-from typing import Any, List, Tuple
-from monograd.utils import dbg
+from typing import List, Tuple
+from monograd.utils import dbg, Device
 import monograd.ops as Ops 
+import numpy as np 
 
 # TODO: for tensor, impl magic methods for things like (1 + tensor)
+# TODO: Lazy ops like tinygrad https://docs.tinygrad.org/quickstart/#tensors
+# TODO: mytype everything
 class Tensor():
     op:Ops.OP
-    data:None
-    metadata:Tuple
-    parents:List|None
+    data:np.ndarray # for now we save it as ndarray but slow
+    parents:Tuple|None
+    device:Device
+    requires_grad:bool
+    parents:Tuple|None
+    name:str|None
+    ctx:object|None
 
-    def __init__(self, op:Ops.OP, data:Any, metadata:Any, parents:List|None):
-        assert op
+    def __init__(self, op:Ops.OP, data:List|np.ndarray, parents:Tuple|None, requires_grad:bool=False, _dtype=np.float32):
+        self.name:str|None = None
+        self.op = op
 
+        # data & grad
+        if isinstance(data, List) or isinstance(data, Tuple):
+            self.data = np.array(data, dtype=_dtype)
+        assert isinstance(data, np.ndarray)
+        self.grad:Tensor|None = None
+        self.requires_grad = requires_grad
+
+        if not parents: self.parents = tuple()
+        else: self.parents = tuple(parents)
+        self.shape = data.shape
+        self.strides = data.strides
+        self.offset = 0
+
+        self.device = Device.CPU
+        self.ctx = None # mask for ops's optimization
+        
--- monograd/utils.py
+++ monograd/utils.py
@@ -1,6 +1,13 @@
+from enum import Enum
 import os
 
 DEBUG_MODE = os.environ.get("DEBUG", "").lower() in ("true", "1", "t")
 def dbg(*args, **kwargs):
     if DEBUG_MODE:
         print(*args, **kwargs)
+
+class Device(Enum):
+    CPU = 1
+    GPU = 2
+    METAL = 3
+
