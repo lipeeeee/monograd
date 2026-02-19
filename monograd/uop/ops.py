a0e9a274d654a8980a193928c7ba11d74bfcdce0
lipeeeee <filipe.pereirafga@gmail.com>
2026-02-19 17:50:42 +0000

    UOps can now have buffers!

--- monograd/uop/ops.py
+++ monograd/uop/ops.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import Any
 import weakref
-from monograd.device import Device
+from monograd.device import Buffer, Device
 from monograd.dtype import DType, dtypes
 from monograd.uop import Ops
 
@@ -14,6 +14,11 @@ class UOpMetaClass(type):
     UOpMetaClass.ucache[args] = ret = super().__call__(*args)
     return ret
 
+# NOTE: storing buffers as pointers outside UOp is kinda sad but it has to be done like this
+# tinygrad and pytorch also solve this issue like this :/
+# monograd's solution could've been to make an OP specifically to store buffers and send them through UOp.src
+_uop_buffers:weakref.WeakKeyDictionary[UOp, Buffer] = weakref.WeakKeyDictionary()
+
 @dataclass(eq=True, slots=True, weakref_slot=True)
 class UOp(metaclass=UOpMetaClass): 
   op: Ops
@@ -23,11 +28,18 @@ class UOp(metaclass=UOpMetaClass):
 
   @property
   def device(self) -> Device:
-    if self.op is Ops.LOAD: return self.arg.device
+    if self.op is Ops.LOAD: return _uop_buffers[self].device
     if self.op is Ops.COPY: return self.arg
     raise NotImplementedError("unkown op")
   @property
   def shape(self) -> tuple:
-    pass
+    if self.op is Ops.LOAD: return self.arg
+    raise NotImplementedError("unkown op")
+  def assign_buffer(self, device:Device, size:int, initial_value=None) -> Buffer:
+    if (dret:=_uop_buffers.get(self, None)) is not None: return dret
+    ret = Buffer(device, size, self.dtype)
+    ret.allocate(initial_value)
+    _uop_buffers[self] = ret
+    return ret
   def __repr__(self):
     return f"<UOp {self.op} dtype={self.dtype.name} arg={self.arg}>"
