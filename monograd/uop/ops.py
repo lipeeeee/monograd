91a3756cfdacc96c5a067d5fbd493a4b3a1f73d5
lipeeeee <filipe.pereirafga@gmail.com>
2026-02-19 00:47:17 +0000

    allow weakrefs on ops

--- monograd/uop/ops.py
+++ monograd/uop/ops.py
@@ -1,28 +1,33 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from typing import Any
+import weakref
 from monograd.device import Device
 from monograd.dtype import DType, dtypes
 from monograd.uop import Ops
 
 # only allow unique UOps
 class UOpMetaClass(type):
-  ucache: dict[tuple, UOp] = {}
+  ucache: dict[tuple, weakref.ReferenceType[UOp]] = weakref.WeakValueDictionary()
   def __call__(self, *args, **kwds):
     if (ret:=UOpMetaClass.ucache.get(args, None)) is not None: return ret
     UOpMetaClass.ucache[args] = ret = super().__call__(*args)
     return ret
 
-@dataclass(eq=True, slots=True)
+@dataclass(eq=True, slots=True, weakref_slot=True)
 class UOp(metaclass=UOpMetaClass): 
   op: Ops
   dtype: DType = dtypes.void
   src: tuple[UOp, ...] = tuple()
-  arg:Any = None
+  arg: Any = None
 
   @property
-  def device(self) -> Device: # we try to get device from src
-    # this is much faster than doing loops
-    # if self.src[0].device is not None: return self.src[0].device
-    # if self.src[1].device is not None: return self.src[1].device
-    raise NotImplementedError("maybe we need a direct reference to device in tensor or here")
+  def device(self) -> Device:
+    if self.op is Ops.LOAD: return self.arg.device
+    if self.op is Ops.COPY: return self.arg
+    raise NotImplementedError("unkown op")
+  @property
+  def shape(self) -> tuple:
+    pass
+  def __repr__(self):
+    return f"<UOp {self.op} dtype={self.dtype.name} arg={self.arg}>"
